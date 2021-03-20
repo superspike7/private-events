acb533c97d500454a0265185ff715312a782cbdf
Spike Vinz Cruz <spikevinz@gmail.com>
2021-03-20 10:50:41 +0800

    add view and controller for events

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,32 @@
+class EventsController < ApplicationController
+
+  def new
+    @event = current_user.created_events.build
+  end
+
+  def create  
+    @event = current_user.created_events.build(user_params)
+    if @event.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+
+  end
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:event).permit(:name, :location)
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   
   resources :users
   resources :sessions
+  resources :events
 
 end
