b4dda1ec759b2fea815d072893ffe881a006eef6
Spike Vinz Cruz <spikevinz@gmail.com>
2021-03-22 09:26:43 +0800

    add scopes past and upcoming events, change view show

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, foreign_key: 'creator_id', class_name: 'User'
   has_many :event_attendings, foreign_key: :attended_event_id
   has_many :attendees, through: :event_attendings, source: :event_attendee 
+
+  scope :upcoming, ->{ where('schedule > ?', DateTime.now ) }
+  scope :past, ->{ where('schedule < ?', DateTime.now ) }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_21_235701) do
+ActiveRecord::Schema.define(version: 2021_03_22_003248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_03_21_235701) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "creator_id"
+    t.datetime "schedule"
   end
 
   create_table "users", force: :cascade do |t|
